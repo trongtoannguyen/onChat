8481237d3e0e0428565b65274ee5f302a57579b7
trongtoan1609ht <trongtoan1609ht@gmail.com>
2024-07-16 07:04:50 +0000

    Lifting state up

--- onChat/src/components/tictac/Board.jsx
+++ onChat/src/components/tictac/Board.jsx
@@ -4,30 +4,32 @@ import "./square.css"
 
 
 export default function Board() {
+    const [values, setValue] = useState(Array(9).fill(null));
+    function handleClick(i) {
+        const nextValues = values.slice();
+        nextValues[i] = "X";
+        setValue(nextValues);
+    }
+
     return <>
         <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
+            <Square value={values[0]} onSquareClick={() => handleClick(0)} />
+            <Square value={values[1]} onSquareClick={() => handleClick(1)} />
+            <Square value={values[2]} onSquareClick={() => handleClick(2)} />
         </div>
         <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
+            <Square value={values[3]} onSquareClick={() => handleClick(3)} />
+            <Square value={values[4]} onSquareClick={() => handleClick(4)} />
+            <Square value={values[5]} onSquareClick={() => handleClick(5)} />
         </div>
         <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
+            <Square value={values[6]} onSquareClick={() => handleClick(6)} />
+            <Square value={values[7]} onSquareClick={() => handleClick(7)} />
+            <Square value={values[8]} onSquareClick={() => handleClick(8)} />
         </div>
     </>
 }
 
-function Square() {
-
-    const [value, setValue] = useState(null);
-    function handleClick() {
-        setValue("X");
-    }
-    return <div className="square" onClick={handleClick}>{value}</div>;
+function Square({ value, onSquareClick }) {
+    return <div className="square" onClick={onSquareClick}>{value}</div>;
 }
